e4e964a83085f3dc0832bd6c80406b1b62c0d189
Danijel Galić <danijel.galic@live.com>
2026-08-27 15:59:52 +0200

    Add utility methods to `DateTime` for convenient instance creation and temporal calculations
    - **New Static Utility Methods**:
      - Added the following static methods to `DateTime` for simplified creation and calculations:
        - `now()`, `today()`, `yesterday()`, `tomorrow()` for quick instance creation based on current, past, or future dates and times.
        - `relative()` for creating instances relative to the current date and time by adding/subtracting time units.
        - `firstDayOfMonth()`, `lastDayOfMonth()` for generating instances representing the start or end of a given month.
        - `firstDayOf()` and `lastDayOf()` for handling relative month calculations (e.g., "next month").
    
    - **Enhanced `Temporal` API**:
      - Extended the flexibility of `DateTime` with expressions and enums like `Keyword`, `DateKeyword`, `TimeKeyword`, `Ordinal`, `Relative`, and `Unit`.
      - Improved support for relative and formatted input interpretations.
    
    - **Error Handling**:
      - Integrated robust exception handling for edge cases such as invalid inputs, ensuring consistent and clear error messaging.
    
    - **Documentation Updates**:
      - Included detailed doc comments for all new methods, explaining parameters, return values, and usage examples.
    
    This update significantly simplifies common date and time operations, enhancing the usability and consistency of the `DateTime` class while maintaining extensibility for advanced temporal logic.

--- src/Temporal/DateTime.php
+++ src/Temporal/DateTime.php
@@ -15,8 +15,11 @@ namespace FireHub\Foundation\Temporal;
 
 use FireHub\Core\Type\Temporal\DateTime as BaseDateTime;
 use FireHub\Core\Type\Date\Zone;
+use FireHub\Core\Meta\Enum\Date\Expression\ {
+    DateKeyword, Keyword, Ordinal, Relative, TimeKeyword,
+};
 use FireHub\Core\Meta\Enum\Date\ {
-    Format\Token, Format
+    Format\Token, Format, Month, Unit
 };
 use FireHub\Foundation\Temporal\DateTime\ {
     Accessors, Inspection, Mutators
@@ -236,6 +239,364 @@ readonly class DateTime extends BaseDateTime {
         );
 
         return new static($datetime);
+
+    }
+
+    /**
+     * ### Creates a new DateTime instance with the current date and time
+     *
+     * <code>
+     * use FireHub\Foundation\Temporal\DateTime;
+     *
+     * $datetime = DateTime::now()->value();
+     *
+     * // current date and time
+     * </code>
+     *
+     * @since 1.0.0
+     *
+     * @uses static::from() To create a new DateTime instance.
+     * @uses \FireHub\Core\Meta\Enum\Date\Expression\Keyword::NOW To get the current date and time.
+     *
+     * @param null|TimezoneType $timezone [optional] <p>
+     * The timezone used to parse the datetime value.
+     * </p>
+     *
+     * @throws \FireHub\Core\Exception\FireHubException If the condition is not met.
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException If the exception is not a FireHubException.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidDateTimeException If the datetime string is invalid.
+     *
+     * @return static<non-empty-string> Returns a new DateTime instance.
+     */
+    public static function now (null|NamedTimezone|FixedTimezone $timezone = null):static {
+
+        return static::from(
+            Keyword::NOW->value,
+            $timezone
+        );
+
+    }
+
+    /**
+     * ### Creates a new DateTime instance with the current date
+     *
+     * <code>
+     * use FireHub\Foundation\Temporal\DateTime;
+     * use FireHub\Core\Meta\Enum\Date\Expression\TimeKeyword;
+     *
+     * $datetime = DateTime::today(TimeKeyword::NOON)->value();
+     *
+     * // today date and noon time
+     * </code>
+     *
+     * @since 1.0.0
+     *
+     * @uses static::from() To create a new DateTime instance.
+     * @uses \FireHub\Core\Meta\Enum\Date\Expression\TimeKeyword::TODAY To get the current calendar day.
+     *
+     * @param null|TimezoneType $timezone [optional] <p>
+     * The timezone used to parse the datetime value.
+     * </p>
+     *
+     * @throws \FireHub\Core\Exception\FireHubException If the condition is not met.
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException If the exception is not a FireHubException.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidDateTimeException If the datetime string is invalid.
+     *
+     * @return static<non-empty-string> Returns a new DateTime instance.
+     */
+    public static function today (string|TimeKeyword $at = TimeKeyword::MIDNIGHT, null|NamedTimezone|FixedTimezone $timezone = null):static {
+
+        return static::from(
+            DateKeyword::TODAY->value.' '.($at instanceof TimeKeyword ? $at->value : $at),
+            $timezone
+        );
+
+    }
+
+    /**
+     * ### Creates a new DateTime instance with the yesterday date
+     *
+     * <code>
+     * use FireHub\Foundation\Temporal\DateTime;
+     * use FireHub\Core\Meta\Enum\Date\Expression\TimeKeyword;
+     *
+     * $datetime = DateTime::yesterday(TimeKeyword::NOON)->value();
+     *
+     * // yesterday date and noon time
+     * </code>
+     *
+     * @since 1.0.0
+     *
+     * @uses static::from() To create a new DateTime instance.
+     * @uses \FireHub\Core\Meta\Enum\Date\Expression\TimeKeyword::YESTERDAY To get the yesterday calendar day.
+     *
+     * @param null|TimezoneType $timezone [optional] <p>
+     * The timezone used to parse the datetime value.
+     * </p>
+     *
+     * @throws \FireHub\Core\Exception\FireHubException If the condition is not met.
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException If the exception is not a FireHubException.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidDateTimeException If the datetime string is invalid.
+     *
+     * @return static<non-empty-string> Returns a new DateTime instance.
+     */
+    public static function yesterday (string|TimeKeyword $at = TimeKeyword::MIDNIGHT, null|NamedTimezone|FixedTimezone $timezone = null):static {
+
+        return static::from(
+            DateKeyword::YESTERDAY->value.' '.($at instanceof TimeKeyword ? $at->value : $at),
+            $timezone
+        );
+
+    }
+
+    /**
+     * ### Creates a new DateTime instance with the tomorrow date
+     *
+     * <code>
+     * use FireHub\Foundation\Temporal\DateTime;
+     * use FireHub\Core\Meta\Enum\Date\Expression\TimeKeyword;
+     *
+     * $datetime = DateTime::tomorrow(TimeKeyword::NOON)->value();
+     *
+     * // tomorrow date and noon time
+     * </code>
+     *
+     * @since 1.0.0
+     *
+     * @uses static::from() To create a new DateTime instance.
+     * @uses \FireHub\Core\Meta\Enum\Date\Expression\TimeKeyword::TOMORROW To get the tomorrow calendar day.
+     *
+     * @param null|TimezoneType $timezone [optional] <p>
+     * The timezone used to parse the datetime value.
+     * </p>
+     *
+     * @throws \FireHub\Core\Exception\FireHubException If the condition is not met.
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException If the exception is not a FireHubException.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidDateTimeException If the datetime string is invalid.
+     *
+     * @return static<non-empty-string> Returns a new DateTime instance.
+     */
+    public static function tomorrow (string|TimeKeyword $at = TimeKeyword::MIDNIGHT, null|NamedTimezone|FixedTimezone $timezone = null):static {
+
+        return static::from(
+            DateKeyword::TOMORROW->value.' '.($at instanceof TimeKeyword ? $at->value : $at),
+            $timezone
+        );
+
+    }
+
+    /**
+     * ### Creates a new DateTime instance with the date and time relative to the current date and time
+     *
+     * <code>
+     * use FireHub\Foundation\Temporal\DateTime;
+     * use FireHub\Core\Meta\Enum\Date\Unit;
+     *
+     * $datetime = DateTime::relative(2, Unit::HOUR)->value();
+     *
+     * // current datetime with 2 hours added
+     * </code>
+     *
+     * @since 1.0.0
+     *
+     * @uses static::from() To create a new DateTime instance.
+     * @uses \FireHub\Core\Meta\Enum\Date\Expression\Keyword::NOW To get the current date and time.
+     *
+     * @param int $number <p>
+     * The number of units to add to the current date and time.
+     * </p>
+     * @param \FireHub\Core\Meta\Enum\Date\Unit $unit <p>
+     * The unit of time to add to the current date and time.
+     * </p>
+     * @param null|TimezoneType $timezone [optional] <p>
+     * The timezone used to parse the datetime value.
+     * </p>
+     *
+     * @throws \FireHub\Core\Exception\FireHubException If the condition is not met.
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException If the exception is not a FireHubException.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidDateTimeException If the datetime string is invalid.
+     *
+     * @return static<non-empty-string> Returns a new DateTime instance.
+     */
+    public static function relative (int $number, Unit $unit, null|NamedTimezone|FixedTimezone $timezone = null):static {
+
+        if ($unit->base() !== null) {
+
+            $number *= $unit->factor();
+            $unit = $unit->base();
+
+        }
+
+        return static::from(
+            Keyword::NOW->value.' '.$number.' '.$unit->name,
+            $timezone
+        );
+
+    }
+
+    /**
+     * ### Creates a new DateTime instance with the first day of the month
+     *
+     * <code>
+     * use FireHub\Foundation\Temporal\DateTime;
+     * use FireHub\Core\Meta\Enum\Date\Month;
+     *
+     * $datetime = DateTime::firstDayOfMonth(Month::NOVEMBER, 2000)->value();
+     *
+     * // first day of November 2000
+     * </code>
+     *
+     * @since 1.0.0
+     *
+     * @uses static::from() To create a new DateTime instance.
+     * @uses \FireHub\Core\Meta\Enum\Date\Expression\Ordinal::FIRST To get the first day of the month.
+     * @uses \FireHub\Core\Meta\Enum\Date\Month::longName() To get the long name of the month.
+     *
+     * @param \FireHub\Core\Meta\Enum\Date\Month $month <p>
+     * The month of the year.
+     * </p>
+     * @param null|int<-9999,9999> $year [optional] <p>
+     * The year of the month.
+     * </p>
+     * @param null|TimezoneType $timezone [optional] <p>
+     * The timezone used to parse the datetime value.
+     * </p>
+     *
+     * @throws \FireHub\Core\Exception\FireHubException If the condition is not met.
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException If the exception is not a FireHubException.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidDateTimeException If the datetime string is invalid.
+     *
+     * @return static<non-empty-string> Returns a new DateTime instance.
+     */
+    public static function firstDayOfMonth (Month $month, ?int $year = null, null|NamedTimezone|FixedTimezone $timezone = null):static {
+
+        return static::from(
+            Ordinal::FIRST->value. ' day of '.$month->longName().' '.$year.' 00:00:00',
+            $timezone
+        );
+
+    }
+
+    /**
+     * ### Creates a new DateTime instance with the last day of the month
+     *
+     * <code>
+     * use FireHub\Foundation\Temporal\DateTime;
+     * use FireHub\Core\Meta\Enum\Date\Month;
+     *
+     * $datetime = DateTime::lastDayOfMonth(Month::NOVEMBER, 2000)->value();
+     *
+     * // last day of November 2000
+     * </code>
+     *
+     * @since 1.0.0
+     *
+     * @uses static::from() To create a new DateTime instance.
+     * @uses \FireHub\Core\Meta\Enum\Date\Expression\Relative::LAST To get the last day of the month.
+     * @uses \FireHub\Core\Meta\Enum\Date\Month::longName() To get the long name of the month.
+     *
+     * @param \FireHub\Core\Meta\Enum\Date\Month $month <p>
+     * The month of the year.
+     * </p>
+     * @param null|int<-9999,9999> $year [optional] <p>
+     * The year of the month.
+     * </p>
+     * @param null|TimezoneType $timezone [optional] <p>
+     * The timezone used to parse the datetime value.
+     * </p>
+     *
+     * @throws \FireHub\Core\Exception\FireHubException If the condition is not met.
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException If the exception is not a FireHubException.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidDateTimeException If the datetime string is invalid.
+     *
+     * @return static<non-empty-string> Returns a new DateTime instance.
+     */
+    public static function lastDayOfMonth (Month $month, ?int $year = null, null|NamedTimezone|FixedTimezone $timezone = null):static {
+
+        return static::from(
+            Relative::LAST->value. ' day of '.$month->longName().' '.$year.' 00:00:00',
+            $timezone
+        );
+
+    }
+
+    /**
+     * ### Creates a new DateTime instance with the first day of a relative month
+     *
+     * <code>
+     * use FireHub\Foundation\Temporal\DateTime;
+     * use FireHub\Core\Meta\Enum\Date\Expression\Relative;
+     *
+     * $datetime = DateTime::firstDayOf(Relative::NEXT)->value();
+     *
+     * // first day of the current year
+     * </code>
+     *
+     * @since 1.0.0
+     *
+     * @uses static::from() To create a new DateTime instance.
+     * @uses \FireHub\Core\Meta\Enum\Date\Expression\Ordinal::FIRST To get the first day of the month.
+     * @uses \FireHub\Core\Meta\Enum\Date\Month::longName() To get the long name of the month.
+     *
+     * @param \FireHub\Core\Meta\Enum\Date\Expression\Relative $month <p>
+     * The relative month.
+     * </p>
+     * @param null|TimezoneType $timezone [optional] <p>
+     * The timezone used to parse the datetime value.
+     * </p>
+     *
+     * @throws \FireHub\Core\Exception\FireHubException If the condition is not met.
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException If the exception is not a FireHubException.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidDateTimeException If the datetime string is invalid.
+     *
+     * @return static<non-empty-string> Returns a new DateTime instance.
+     */
+    public static function firstDayOf (Relative $month, null|NamedTimezone|FixedTimezone $timezone = null):static {
+
+        return static::from(
+            Ordinal::FIRST->value. ' day of '.$month->value.' month 00:00:00',
+            $timezone
+        );
+
+    }
+
+    /**
+     * ### Creates a new DateTime instance with the last day of a relative month
+     *
+     * <code>
+     * use FireHub\Foundation\Temporal\DateTime;
+     * use FireHub\Core\Meta\Enum\Date\Expression\Relative;
+     *
+     * $datetime = DateTime::lastDayOf(Relative::NEXT)->value();
+     *
+     * // last day of the current year
+     * </code>
+     *
+     * @since 1.0.0
+     *
+     * @uses static::from() To create a new DateTime instance.
+     * @uses \FireHub\Core\Meta\Enum\Date\Expression\Relative::LAST To get the last day of the month.
+     * @uses \FireHub\Core\Meta\Enum\Date\Month::longName() To get the long name of the month.
+     *
+     * @param \FireHub\Core\Meta\Enum\Date\Expression\Relative $month <p>
+     * The relative month.
+     * </p>
+     * @param null|TimezoneType $timezone [optional] <p>
+     * The timezone used to parse the datetime value.
+     * </p>
+     *
+     * @throws \FireHub\Core\Exception\FireHubException If the condition is not met.
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException If the exception is not a FireHubException.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidDateTimeException If the datetime string is invalid.
+     *
+     * @return static<non-empty-string> Returns a new DateTime instance.
+     */
+    public static function lastDayOf (Relative $month, null|NamedTimezone|FixedTimezone $timezone = null):static {
+
+        return static::from(
+            Relative::LAST->value. ' day of '.$month->value.' month 00:00:00',
+            $timezone
+        );
 
     }
 
