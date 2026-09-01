f5105462747b2318d3955c5e5b815316b5573401
Danijel Galić <danijel.galic@live.com>
2026-09-01 10:27:20 +0200

    Add validation to `Period` with `InvalidPeriodException` and implement start/end modification methods
    - **New Exception**:
      - Introduced `InvalidPeriodException` for handling invalid start and end date configurations in `Period`.
    
    - **Enhancements in `Period`**:
      - Added validation to the constructor to ensure the start date is before the end date:
        - Throws `InvalidPeriodException` if the validation fails.
    
    - **New Methods**:
      - `reduceStart()`, `extendStart()`, `reduceEnd()`, `extendEnd()`:
        - Provide functionality to modify the start or end of the period by a given `Timespan`.
    
    - **Unit Tests**:
      - Enhanced `PeriodTest` to include:
        - Validation for invalid periods that raise `InvalidPeriodException`.
        - Coverage for all new methods (`reduceStart`, `extendStart`, `reduceEnd`, `extendEnd`) with expected outputs and exception cases.
    
    This update reinforces the `Period` class with robust validation and powerful mutator methods, improving reliability and enabling advanced temporal period adjustments.

--- src/Temporal/Period.php
+++ src/Temporal/Period.php
@@ -14,8 +14,9 @@
 namespace FireHub\Foundation\Temporal;
 
 use FireHub\Core\Type\Temporal\ {
-    DateTime as BaseDateTime, Period as BasePeriod
+    DateTime as BaseDateTime, Period as BasePeriod, Timespan as BaseTimespan
 };
+use FireHub\Foundation\Temporal\Exception\InvalidPeriodException;
 
 /**
  * ### Provides an immutable temporal Period Value Object with a high-level developer API
@@ -41,6 +42,10 @@ readonly class Period extends BasePeriod {
      * ### Constructor
      * @since 1.0.0
      *
+     * @uses \FireHub\Core\Type\ValueObject::guard() As a guard.
+     * @uses \FireHub\Foundation\Temporal\DateTime::isBefore() To check if the start date and time is before the end
+     * date and time.
+     *
      * @param \FireHub\Foundation\Temporal\DateTime<non-empty-string> $start <p>
      * The start date and time of the period.
      * </p>
@@ -48,12 +53,26 @@ readonly class Period extends BasePeriod {
      * The end date and time of the period.
      * </p>
      *
+     * @throws \FireHub\Core\Exception\FireHubException If the condition is not met.
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException If the exception is not a FireHubException.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidPeriodException If the start date and time is after the
+     * end date and time.
+     *
      * @return void
      */
     public function __construct (
         protected DateTime $start,
         protected DateTime $end
-    ) {}
+    ) {
+
+        $this->guard(
+            fn ():bool => $start->isBefore($end),
+            fn ():InvalidPeriodException => new InvalidPeriodException(
+                'The start date and time cannot be after the end date and time.'
+            )
+        );
+
+    }
 
     /**
      * ### Checks if a given DateTime object is within the period
@@ -62,10 +81,10 @@ readonly class Period extends BasePeriod {
      * use FireHub\Foundation\Temporal\Period;
      * use FireHub\Foundation\Temporal\DateTime;
      *
-     * $datetime = new Period(
-     *     new DateTime('2000-01-01 12:00:00.111111'),
-     *     new DateTime('2021-05-01 18:30:00.123456')
-     * )->inPeriod(new DateTime('2000-01-01 12:00:00.111111'));
+     * $datetime = new DateTime('2000-01-01 12:00:00.111111');
+     * $datetime2 = new DateTime('2021-05-01 18:30:00.123456');
+     *
+     * $datetime = new Period($datetime, $datetime2)->value();
      *
      * // true
      * </code>
@@ -91,10 +110,10 @@ readonly class Period extends BasePeriod {
      * use FireHub\Foundation\Temporal\Period;
      * use FireHub\Foundation\Temporal\DateTime;
      *
-     * $datetime = new Period(
-     *     new DateTime('2000-01-01 12:00:00.111111'),
-     *     new DateTime('2021-05-01 18:30:00.123456')
-     * )->inProgress();
+     * $datetime = new DateTime('2000-01-01 12:00:00.111111');
+     * $datetime2 = new DateTime('2021-05-01 18:30:00.123456');
+     *
+     * $datetime = new Period($datetime, $datetime2)->value();
      *
      * // false
      * </code>
@@ -120,12 +139,12 @@ readonly class Period extends BasePeriod {
      * use FireHub\Foundation\Temporal\Period;
      * use FireHub\Foundation\Temporal\DateTime;
      *
-     * $datetime = new Period(
-     *     new DateTime('2000-01-01 12:00:00.111111'),
-     *     new DateTime('2021-05-01 18:30:00.123456')
-     * )->value();
+     * $datetime = new DateTime('2000-01-01 12:00:00.111111');
+     * $datetime2 = new DateTime('2021-05-01 18:30:00.123456');
      *
-     * // '2000-01-01 12:00:00.000000'
+     * $datetime = new Period($datetime, $datetime2)->value();
+     *
+     * // '2000-01-01 12:00:00.111111 - 2021-05-01 18:30:00.123456'
      * </code>
      *
      * @since 1.0.0
@@ -137,6 +156,162 @@ readonly class Period extends BasePeriod {
     public function value ():string {
 
         return $this->start->value() . ' - ' . $this->end->value();
+
+    }
+
+    /**
+     * ### Reduces the start date and time by a given timespan
+     *
+     * <code>
+     * use FireHub\Foundation\Temporal\Period;
+     * use FireHub\Foundation\Temporal\DateTime;
+     * use FireHub\Foundation\Temporal\Timespan;
+     *
+     * $datetime = new DateTime('2000-01-01 12:00:00.000000');
+     * $datetime2 = new DateTime('2001-01-01 12:00:00.000000');
+     * $timespan = new Timespan('0')->addDays('1')->addHours('12')->addMicroseconds('123456');
+     *
+     * $datetime = new Period($datetime, $datetime2)->reduceStart($timespan);
+     *
+     * // '1999-12-30 23:59:59.876544 - 2021-01-01 12:00:00.000000'
+     * </code>
+     *
+     * @since 1.0.0
+     *
+     * @uses \FireHub\Foundation\Temporal\DateTime::sub() To reduce the start date and time by a given timespan.
+     *
+     * @param \FireHub\Core\Type\Temporal\Timespan<numeric-string> $timespan <p>
+     * The timespan to reduce the start date and time by.
+     * </p>
+     *
+     * @throws \FireHub\Core\Exception\FireHubException If the condition is not met.
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException If the exception is not a FireHubException.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidPeriodException If the start date and time is after the
+     * end date and time.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidTimespanComponents If the components are invalid.
+     *
+     * @return static<non-empty-string> A new Period object with the start date and time reduced by the given timespan.
+     */
+    public function reduceStart (BaseTimespan $timespan):static {
+
+        return new static($this->start->sub($timespan), $this->end);
+
+    }
+
+    /**
+     * ### Extends the start date and time by a given timespan
+     *
+     * <code>
+     * use FireHub\Foundation\Temporal\Period;
+     * use FireHub\Foundation\Temporal\DateTime;
+     * use FireHub\Foundation\Temporal\Timespan;
+     *
+     * $datetime = new DateTime('2000-01-01 12:00:00.000000');
+     * $datetime2 = new DateTime('2001-01-01 12:00:00.000000');
+     * $timespan = new Timespan('0')->addDays('1')->addHours('12')->addMicroseconds('123456');
+     *
+     * $datetime = new Period($datetime, $datetime2)->extendStart($timespan);
+     *
+     * // '2000-01-03 00:00:00.123456 - 2021-01-01 12:00:00.000000'
+     * </code>
+     *
+     * @since 1.0.0
+     *
+     * @uses \FireHub\Foundation\Temporal\DateTime::add() To extend the start date and time by a given timespan.
+     *
+     * @param \FireHub\Core\Type\Temporal\Timespan<numeric-string> $timespan <p>
+     * The timespan to extend the start date and time by.
+     * </p>
+     *
+     * @throws \FireHub\Core\Exception\FireHubException If the condition is not met.
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException If the exception is not a FireHubException.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidPeriodException If the start date and time is after the
+     * end date and time.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidTimespanComponents If the components are invalid.
+     *
+     * @return static<non-empty-string> A new Period object with the start date and time extended by the given timespan.
+     */
+    public function extendStart (BaseTimespan $timespan):static {
+
+        return new static($this->start->add($timespan), $this->end);
+
+    }
+
+    /**
+     * ### Reduces the end date and time by a given timespan
+     *
+     * <code>
+     * use FireHub\Foundation\Temporal\Period;
+     * use FireHub\Foundation\Temporal\DateTime;
+     * use FireHub\Foundation\Temporal\Timespan;
+     *
+     * $datetime = new DateTime('2000-01-01 12:00:00.000000');
+     * $datetime2 = new DateTime('2001-01-01 12:00:00.000000');
+     * $timespan = new Timespan('0')->addDays('1')->addHours('12')->addMicroseconds('123456');
+     *
+     * $datetime = new Period($datetime, $datetime2)->reduceEnd($timespan);
+     *
+     * // '2000-01-01 12:00:00.000000 - 2020-12-30 23:59:59.876544'
+     * </code>
+     *
+     * @since 1.0.0
+     *
+     * @uses \FireHub\Foundation\Temporal\DateTime::sub() To reduce the end date and time by a given timespan.
+     *
+     * @param \FireHub\Core\Type\Temporal\Timespan<numeric-string> $timespan <p>
+     * The timespan to reduce the end date and time by.
+     * </p>
+     *
+     * @throws \FireHub\Core\Exception\FireHubException If the condition is not met.
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException If the exception is not a FireHubException.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidPeriodException If the start date and time is after the
+     * end date and time.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidTimespanComponents If the components are invalid.
+     *
+     * @return static<non-empty-string> A new Period object with the end date and time reduced by the given timespan.
+     */
+    public function reduceEnd (BaseTimespan $timespan):static {
+
+        return new static($this->start, $this->end->sub($timespan));
+
+    }
+
+    /**
+     * ### Extends the end date and time by a given timespan
+     *
+     * <code>
+     * use FireHub\Foundation\Temporal\Period;
+     * use FireHub\Foundation\Temporal\DateTime;
+     * use FireHub\Foundation\Temporal\Timespan;
+     *
+     * $datetime = new DateTime('2000-01-01 12:00:00.000000');
+     * $datetime2 = new DateTime('2001-01-01 12:00:00.000000');
+     * $timespan = new Timespan('0')->addDays('1')->addHours('12')->addMicroseconds('123456');
+     *
+     * $datetime = new Period($datetime, $datetime2)->extendEnd($timespan);
+     *
+     * // '2000-01-01 12:00:00.000000 - 2021-01-03 00:00:00.123456'
+     * </code>
+     *
+     * @since 1.0.0
+     *
+     * @uses \FireHub\Foundation\Temporal\DateTime::add() To extend the end date and time by a given timespan.
+     *
+     * @param \FireHub\Core\Type\Temporal\Timespan<numeric-string> $timespan <p>
+     * The timespan to extend the end date and time by.
+     * </p>
+     *
+     * @throws \FireHub\Core\Exception\FireHubException If the condition is not met.
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException If the exception is not a FireHubException.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidPeriodException If the start date and time is after the
+     * end date and time.
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidTimespanComponents If the components are invalid.
+     *
+     * @return static<non-empty-string> A new Period object with the end date and time extended by the given timespan.
+     */
+    public function extendEnd (BaseTimespan $timespan):static {
+
+        return new static($this->start, $this->end->add($timespan));
 
     }
 
--- tests/Unit/Temporal/PeriodTest.php
+++ tests/Unit/Temporal/PeriodTest.php
@@ -15,8 +15,9 @@ namespace FireHub\Tests\Foundation\Unit\Temporal;
 
 use FireHub\Testing\FireHubTestCase;
 use FireHub\Foundation\Temporal\ {
-    DateTime, Period
+    DateTime, Period, Timespan
 };
+use FireHub\Foundation\Temporal\Exception\InvalidPeriodException;
 use PHPUnit\Framework\Attributes\ {
     CoversClass, Group, Small, TestWith
 };
@@ -33,6 +34,27 @@ final class PeriodTest extends FireHubTestCase {
     /**
      * @since 1.0.0
      *
+     * @param non-empty-string $start
+     * @param non-empty-string $end
+     *
+     * @throws \FireHub\Core\Exception\FireHubException
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidTimestampException
+     *
+     * @return void
+     */
+    #[TestWith(['2001-01-01 12:00:00', '2000-01-01 12:00:00'])]
+    public function testInvalidPeriod (string $start, string $end):void {
+
+        $this->expectException(InvalidPeriodException::class);
+
+        new Period(DateTime::from($start), DateTime::from($end));
+
+    }
+
+    /**
+     * @since 1.0.0
+     *
      * @param bool $expected
      * @param non-empty-string $start
      * @param non-empty-string $end
@@ -41,6 +63,7 @@ final class PeriodTest extends FireHubTestCase {
      * @throws \FireHub\Core\Exception\FireHubException
      * @throws \FireHub\Core\Type\Exception\ValueObjectException
      * @throws \FireHub\Foundation\Temporal\Exception\InvalidTimestampException
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidPeriodException
      *
      * @return void
      */
@@ -63,6 +86,7 @@ final class PeriodTest extends FireHubTestCase {
      * @throws \FireHub\Core\Exception\FireHubException
      * @throws \FireHub\Core\Type\Exception\ValueObjectException
      * @throws \FireHub\Foundation\Temporal\Exception\InvalidTimestampException
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidPeriodException
      *
      * @return void
      */
@@ -83,6 +107,7 @@ final class PeriodTest extends FireHubTestCase {
      * @throws \FireHub\Core\Exception\FireHubException
      * @throws \FireHub\Core\Type\Exception\ValueObjectException
      * @throws \FireHub\Foundation\Temporal\Exception\InvalidTimestampException
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidPeriodException
      *
      * @return void
      */
@@ -90,6 +115,114 @@ final class PeriodTest extends FireHubTestCase {
     public function testValue (string $expected, string $start, string $end):void {
 
         self::assertSame($expected, new Period(DateTime::from($start), DateTime::from($end))->value());
+
+    }
+
+    /**
+     * @since 1.0.0
+     *
+     * @param non-empty-string $expected
+     * @param non-empty-string $start
+     * @param non-empty-string $end
+     * @param numeric-string $timespan
+     *
+     * @throws \FireHub\Core\Exception\FireHubException
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidTimestampException
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidPeriodException
+     *
+     * @return void
+     */
+    #[TestWith(['1999-12-30 23:59:59.876544 - 2021-01-01 12:00:00.000000', '2000-01-01 12:00:00.000000', '2021-01-01 12:00:00.000000', '129600123456'])]
+    public function testReduceStart (string $expected, string $start, string $end, string $timespan):void {
+
+        self::assertSame(
+            $expected,
+            new Period(
+                DateTime::from($start),
+                DateTime::from($end)
+            )->reduceStart(new Timespan($timespan))->value());
+
+    }
+
+    /**
+     * @since 1.0.0
+     *
+     * @param non-empty-string $expected
+     * @param non-empty-string $start
+     * @param non-empty-string $end
+     * @param numeric-string $timespan
+     *
+     * @throws \FireHub\Core\Exception\FireHubException
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidTimestampException
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidPeriodException
+     *
+     * @return void
+     */
+    #[TestWith(['2000-01-03 00:00:00.123456 - 2021-01-01 12:00:00.000000', '2000-01-01 12:00:00.000000', '2021-01-01 12:00:00.000000', '129600123456'])]
+    public function testExtendStart (string $expected, string $start, string $end, string $timespan):void {
+
+        self::assertSame(
+            $expected,
+            new Period(
+                DateTime::from($start),
+                DateTime::from($end)
+            )->extendStart(new Timespan($timespan))->value());
+
+    }
+
+    /**
+     * @since 1.0.0
+     *
+     * @param non-empty-string $expected
+     * @param non-empty-string $start
+     * @param non-empty-string $end
+     * @param numeric-string $timespan
+     *
+     * @throws \FireHub\Core\Exception\FireHubException
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidTimestampException
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidPeriodException
+     *
+     * @return void
+     */
+    #[TestWith(['2000-01-01 12:00:00.000000 - 2020-12-30 23:59:59.876544', '2000-01-01 12:00:00.000000', '2021-01-01 12:00:00.000000', '129600123456'])]
+    public function testReduceEnd (string $expected, string $start, string $end, string $timespan):void {
+
+        self::assertSame(
+            $expected,
+            new Period(
+                DateTime::from($start),
+                DateTime::from($end)
+            )->reduceEnd(new Timespan($timespan))->value());
+
+    }
+
+    /**
+     * @since 1.0.0
+     *
+     * @param non-empty-string $expected
+     * @param non-empty-string $start
+     * @param non-empty-string $end
+     * @param numeric-string $timespan
+     *
+     * @throws \FireHub\Core\Exception\FireHubException
+     * @throws \FireHub\Core\Type\Exception\ValueObjectException
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidTimestampException
+     * @throws \FireHub\Foundation\Temporal\Exception\InvalidPeriodException
+     *
+     * @return void
+     */
+    #[TestWith(['2000-01-01 12:00:00.000000 - 2021-01-03 00:00:00.123456', '2000-01-01 12:00:00.000000', '2021-01-01 12:00:00.000000', '129600123456'])]
+    public function testExtendEnd (string $expected, string $start, string $end, string $timespan):void {
+
+        self::assertSame(
+            $expected,
+            new Period(
+                DateTime::from($start),
+                DateTime::from($end)
+            )->extendEnd(new Timespan($timespan))->value());
 
     }
 
